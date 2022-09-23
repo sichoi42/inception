68891c5bd3cb5fcf885149bc21d64475941d16fe
sichoi42 <42.4.sichoi@gmail.com>
2022-09-23 13:47:12 +0900

    :sparkles: add admin user in mariadb
    add admin user in mariadb

--- src/requirements/mariadb/tools/init.sh
+++ src/requirements/mariadb/tools/init.sh
@@ -1,14 +1,33 @@
+#!/bin/bash
 
-# # Check Whether Already Set Up or Not by Custom File
-# cat .setup 2> /dev/null
-
-# # If Not Set Up Yet
-# if [ $? -ne 0 ]; then
-
-# sed -i "s/.*bind-address\s*=.*/bind-address=0.0.0.0\nport=3306/g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
-# fi
-
-sleep 5
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
 
 service mysql start
+
+#Check if the database exists
+
+if [ -d "/var/lib/mysql/$MARIADB_DATABASE" ]
+then
+	echo -e "${RED} Database [$MARIADB_DATABASE] already exists ${RESET}"
+else
+	# # Create root user
+	# echo -e "${GREEN} GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'; FLUSH PRIVILEGES; ${RESET}"
+	# echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u$MARIADB_USER -p$MARIADB_PASSWORD
+
+	# Create database and grant all on admin user
+	echo -e "${GREEN} CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE; GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_ADMIN_USER'@'%' IDENTIFIED BY '$MARIADB_ADMIN_PASSWORD'; FLUSH PRIVILEGES; ${RESET}"
+	echo "CREATE DATABASE IF NOT EXISTS $MARIADB_DATABASE; GRANT ALL ON $MARIADB_DATABASE.* TO '$MARIADB_ADMIN_USER'@'%' IDENTIFIED BY '$MARIADB_ADMIN_PASSWORD'; FLUSH PRIVILEGES;" | mysql -u$MARIADB_ADMIN_USER -p$MARIADB_ADMIN_PASSWORD
+
+	# Create normal user
+	echo -e "${GREEN} CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD'; ${RESET}"
+	echo "CREATE USER '$MARIADB_USER'@'%' IDENTIFIED BY '$MARIADB_PASSWORD';" | mysql -u$MARIADB_USER -p$MARIADB_PASSWORD
+
+	# Import database
+	mysql -u$MARIADB_USER -p$MARIADB_PASSWORD $MARIADB_DATABASE < ./my_wordpress.sql
+fi
+
+service mysql stop
+
+exec "$@"
