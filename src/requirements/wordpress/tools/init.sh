607863b725dd2c4ffa8b71d152b9584b819bef30
sichoi <42.4.sichoi@gmail.com>
2022-09-27 21:15:01 +0900

    :memo: using env_file
    using env_file

--- src/requirements/wordpress/tools/init.sh
+++ src/requirements/wordpress/tools/init.sh
@@ -9,7 +9,7 @@ if [ $? -eq 0 ]; then
 #   Change the Listening Host with 9000 Port
 #   sed -i "s/.*listen = 127.0.0.1.*/listen = 9000/g" /etc/php/7.3/fpm/pool.d/www.conf
   # Append Env Variables on the Configuration File
-  echo "env[MARIADB_DATABASE_HOST] = \$MARIADB_DATABASE_HOST" >> /etc/php/7.3/fpm/pool.d/www.conf
+  echo "env[MARIADB_HOST] = \$MARIADB_HOST" >> /etc/php/7.3/fpm/pool.d/www.conf
   echo "env[MARIADB_ADMIN_USER] = \$MARIADB_ADMIN_USER" >> /etc/php/7.3/fpm/pool.d/www.conf
   echo "env[MARIADB_ADMIN_PASSWORD] = \$MARIADB_ADMIN_PASSWORD" >> /etc/php/7.3/fpm/pool.d/www.conf
   echo "env[MARIADB_DATABASE] = \$MARIADB_DATABASE" >> /etc/php/7.3/fpm/pool.d/www.conf
@@ -25,12 +25,12 @@ chown -R :www-data /var/www/wordpress
 
 	# sleep 5
 
-	# if ! mysqladmin -h $MARIADB_DATABASE_HOST -u $MARIADB_ADMIN_USER --password=$MARIADB_ADMIN_PASSWORD --wait=42 ping > /dev/null; then
+	# if ! mysqladmin -h $MARIADB_HOST -u $MARIADB_ADMIN_USER --password=$MARIADB_ADMIN_PASSWORD --wait=42 ping > /dev/null; then
 	# 	echo -e "${RED} Mariadb is not reachable\n ${RESET}"
 	# 	exit 1
 	# fi
 	wp core download --path=/var/www/wordpress --allow-root
-	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_DATABASE_HOST --dbcharset="utf8" --allow-root
+	wp config create --dbname=$MARIADB_DATABASE --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASSWORD --dbhost=$MARIADB_HOST --dbcharset="utf8" --allow-root
 	wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path=/var/www/wordpress --allow-root
 	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 	wp plugin update --all --allow-root
