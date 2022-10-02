fbb486eeb6a8363c4514e38b371734ffcb728fb8
sichoi42 <42.4.sichoi@gmail.com>
2022-10-02 15:37:23 +0900

    :art: .
    .

--- srcs/requirements/bonus/redis/tools/init.sh
+++ srcs/requirements/bonus/redis/tools/init.sh
@@ -2,5 +2,8 @@
 
 chmod 777 /etc/redis/redis.conf
 
+sed -i "s/bind 127.0.0.1/bind 0.0.0.0/g" /etc/redis/redis.conf
+sed -i "s/protected-mode yes/protected-mode no/g" /etc/redis/redis.conf
+
 # Run by Dumb Init
 redis-server /etc/redis/redis.conf
