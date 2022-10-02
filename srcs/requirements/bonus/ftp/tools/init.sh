3aa88be89682e7ca38c515e4460a8d2e8cc18616
sichoi42 <42.4.sichoi@gmail.com>
2022-10-02 18:44:32 +0900

    :art: .

--- srcs/requirements/bonus/ftp/tools/init.sh
+++ srcs/requirements/bonus/ftp/tools/init.sh
@@ -10,7 +10,7 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf" ]; then
 
 	echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-	chown -R $FTP_USER:$FTP_USER /var/www/wordpress
+	chown -R $FTP_USER:$FTP_USER /var/www/html
 
 	echo "ftpd_banner=Welcome to $FTP_USER FTP service!!" >> /etc/vsftpd.conf
 fi
