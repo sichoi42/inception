23a48f68bafe2cbbf2700c350bdd60495d8f285c
sichoi42 <42.4.sichoi@gmail.com>
2022-10-02 15:59:30 +0900

    :art: .
    .

--- srcs/requirements/wordpress/tools/init.sh
+++ srcs/requirements/wordpress/tools/init.sh
@@ -27,7 +27,7 @@ PHP
 wp core install --url="$WP_URL" --title="$WP_TITLE" --admin_user="$WP_ADMIN_USER" --admin_password="$WP_ADMIN_PASSWORD" --admin_email="$WP_ADMIN_EMAIL" --path=/var/www/wordpress --allow-root
 wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASSWORD --allow-root
 wp plugin update --all --allow-root
-wp plugin install redis-cache --active --allow-root
+wp plugin install redis-cache --activate --allow-root
 wp redis enable --allow-root
 
 cp -r /var/www/wordpress /var/www/html/
