c3aece7a6afa44e44e9a71021b39a8f0e5cfeaf8
sichoi42 <42.4.sichoi@gmail.com>
2022-10-15 23:14:59 +0900

    :bug: fix wrong syntax
    fix wrong syntax

--- srcs/requirements/bonus/hugo/tools/install_hugo.sh
+++ srcs/requirements/bonus/hugo/tools/install_hugo.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 arch=$(uname -m)
-if [ arch == 'arm64' ] || [ arch == 'aarch64' ]; then
+
+if [ $arch == "arm64" ] || [ $arch == "aarch64" ]; then
 	wget -O - https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_0.104.3_linux-arm64.tar.gz > hugo.tar.gz
-elif [ arch == 'amd64' ]; then
+elif [ $arch == "amd64" ]; then
 	wget -O - https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_0.104.3_linux-amd64.tar.gz > hugo.tar.gz
 else
 	echo "Unsupported architecture"
