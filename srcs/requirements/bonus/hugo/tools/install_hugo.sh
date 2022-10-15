16ff44caa36e815c9d139f381ca30996dab6aa76
sichoi42 <42.4.sichoi@gmail.com>
2022-10-15 23:07:56 +0900

    :wrench: install hugo corresponding arch
    install hugo corresponding arch

---
+++ srcs/requirements/bonus/hugo/tools/install_hugo.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+arch=$(uname -m)
+if [ arch == 'arm64' ] || [ arch == 'aarch64' ]; then
+	wget -O - https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_0.104.3_linux-arm64.tar.gz > hugo.tar.gz
+elif [ arch == 'amd64' ]; then
+	wget -O - https://github.com/gohugoio/hugo/releases/download/v0.104.3/hugo_0.104.3_linux-amd64.tar.gz > hugo.tar.gz
+
+tar -xvf hugo.tar.gz
+
+chmod +x hugo
+
+mv hugo /usr/local/bin
